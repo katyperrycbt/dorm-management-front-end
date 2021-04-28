6f00f01593a11a590917e83a2beac91547f70edc
khoindd2000 <55172106+khoindd2000@users.noreply.github.com>
2021-04-28 15:05:50 +0700

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,8 @@
 import { combineReducers } from 'redux';
-
-import auth from './auth';
-
-import open from '../components/Request/FixingRoom/FixingRequest/open';
-
 import RequestInfor from '../components/Request/FixingRoom/FixingRequest/RequestInfor';
-
-
-
+import auth from './auth';
+import open from '../components/Request/FixingRoom/FixingRequest/open';
 export default combineReducers({ auth, isOpen: open, RequestList: RequestInfor });
+
+
+
