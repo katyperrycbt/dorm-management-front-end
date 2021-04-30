b65c9238a3a8d2fe69527db7388b7d84d4ae28c0
Dang Khoi Nguyen Duc <khoindd2000@gmail.com>
2021-04-30 21:02:03 +0700

    add link for button in homepage

--- src/components/CustomButton/CustomButton.jsx
+++ src/components/CustomButton/CustomButton.jsx
@@ -2,16 +2,25 @@ import Tab from '@material-ui/core/Tab';
 import IconButton from '@material-ui/core/IconButton';
 import useStyles from './styles';
 
+import { useHistory } from 'react-router-dom';
+
+
 const randomHex = () => {
   return '#' + (Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
 }
 
-const CustomButton = ({ icon, content }) => {
-  
+const CustomButton = ({ icon, content, site }) => {
+  const history = useHistory();
+
   const classes = useStyles();
 
+  const handleClick = (site) => {
+    history.push(`/${site}`);
+    console.log(site)
+  }
+
   return (
-    <div className={classes.root} style={{ background: `linear-gradient(45deg, ${randomHex()} 30%, ${randomHex()} 90%)` }}>
+    <div className={classes.root} onClick={() => handleClick(site)} style={{ background: `linear-gradient(45deg, ${randomHex()} 30%, ${randomHex()} 90%)` }}>
       <IconButton color="primary" className={`${classes.customWidth}`} disableRipple={true} disableFocusRipple={true}>
         <Tab icon={
           icon
