20b3a1f7ec401c88ef1a126724d9a157c8ba9a83
Dang Khoi Nguyen Duc <khoindd2000@gmail.com>
2021-05-24 21:39:14 +0700

    add admin information and fix admin sentence in MainSideMenu

--- src/App.jsx
+++ src/App.jsx
@@ -16,14 +16,15 @@ import Bills from "./components/Bills/Bills";
 import Requests from "./components/Request/Requests";
 import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
-import Dashboard from './components/Dashboard/Dashboard';
+import Dashboard from "./components/Dashboard/Dashboard";
+import Admins from "./components/Admins/Admins";
 
 const App = () => {
   const dispatch = useDispatch();
   const [linear, setLinear] = useState(false);
   const classes = useStyles();
 
-  const admin = window.location.href.indexOf('admin.iu-dormitory.fun') === 8;
+  const admin = !(window.location.href.indexOf("admin.iu-dormitory.fun") === 8);
 
   if (admin) {
     return (
@@ -40,9 +41,26 @@ const App = () => {
           <MainMenu />
           <Switch>
             <Route exact path="/" render={(props) => <Home {...props} />} />
-            <Route exact path="/login" render={(props) => <Auth {...props} />} />
-            <Route exact path="/info" render={(props) => <Personal {...props} />} />
-            <Route exact path="/dashboard" render={(props) => <Dashboard {...props} />} />
+            <Route
+              exact
+              path="/login"
+              render={(props) => <Auth {...props} />}
+            />
+            <Route
+              exact
+              path="/info"
+              render={(props) => <Personal {...props} />}
+            />
+            <Route
+              exact
+              path="/dashboard"
+              render={(props) => <Dashboard {...props} />}
+            />
+            <Route
+              exact
+              path="/admins"
+              render={(props) => <Admins {...props} />}
+            />
           </Switch>
         </Container>
       </BrowserRouter>
