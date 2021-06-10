5cd2e4762a37b17fac6360f7badd2550248a7c4c
Tran Trong Thuc <katyperrycbt@gmail.com>
2021-06-10 23:06:30 +0700

    UD

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ const App = () => {
   const handleClose = () => {
     dispatch({ type: SET_SNACK, data: { open: false, msg: '' } });
   }
-
+//test source tree
   if (admin) {
     return (
       <BrowserRouter>
