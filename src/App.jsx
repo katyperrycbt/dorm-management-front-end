f6b3a4f42eefd728a3503e0a8c1aa0f8fb73a96c
khoindd <khoindd2000@gmail.com>
2021-06-10 23:08:29 +0700

    CheckSourcetree

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import LinearProgress from "@material-ui/core/LinearProgress";
 import { useDispatch } from "react-redux";
 
 import useStyles from "./styles";
-import './App.css';
+import "./App.css";
 
 import MainMenu from "./components/Menu/MainMenu";
 
@@ -15,13 +15,13 @@ import Bills from "./components/Bills/Bills";
 import Requests from "./components/Request/Requests";
 import About from "./components/About/Alter";
 import Contact from "./components/Contact/Contact";
-import Dashboard from './components/Dashboard2/Dashboard';
-import Login from './components/Login/Login';
-import { Snackbar } from '@material-ui/core';
-import { useSelector } from 'react-redux';
-import { SET_SNACK } from './constants/constants';
-import Landing from './Landing';
-import Notification from './components/Notification/Notification';
+import Dashboard from "./components/Dashboard2/Dashboard";
+import Login from "./components/Login/Login";
+import { Snackbar } from "@material-ui/core";
+import { useSelector } from "react-redux";
+import { SET_SNACK } from "./constants/constants";
+import Landing from "./Landing";
+import Notification from "./components/Notification/Notification";
 
 const App = () => {
   const dispatch = useDispatch();
@@ -33,27 +33,51 @@ const App = () => {
 
   const linear = useSelector((state) => state.linear);
 
-  const getIndex = window.location.href.indexOf('admin');
+  const getIndex = window.location.href.indexOf("admin");
   const admin = getIndex === 8 || getIndex === 7;
 
-
   const handleClose = () => {
-    dispatch({ type: SET_SNACK, data: { open: false, msg: '' } });
-  }
-//test source tree
+    dispatch({ type: SET_SNACK, data: { open: false, msg: "" } });
+  };
+  //test source tree
   if (admin) {
     return (
       <BrowserRouter>
-        <Snackbar anchorOrigin={{ vertical: 'top', horizontal: 'right', zIndex: 10000000 }} open={snack} onClose={handleClose} message={snackMSG} />
-        {
-          linear && <div className={classes.linearProgress} style={{ position: 'fixed', top: 0, zIndex: 10000000 }}>
-            <LinearProgress color="secondary" classes={{ colorSecondary: classes.customColor, barColorSecondary: classes.customColor2 }} />
+        <Snackbar
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "right",
+            zIndex: 10000000,
+          }}
+          open={snack}
+          onClose={handleClose}
+          message={snackMSG}
+        />
+        {linear && (
+          <div
+            className={classes.linearProgress}
+            style={{ position: "fixed", top: 0, zIndex: 10000000 }}
+          >
+            <LinearProgress
+              color="secondary"
+              classes={{
+                colorSecondary: classes.customColor,
+                barColorSecondary: classes.customColor2,
+              }}
+            />
           </div>
-        }
+        )}
         <Container maxWidth="lg">
           <Switch>
-            <Route path="/:any(me|new|email|)" render={(props) => <Dashboard {...props} />} />
-            <Route exact path="/login" render={(props) => <Login {...props} />} />
+            <Route
+              path="/:any(me|new|email|)"
+              render={(props) => <Dashboard {...props} />}
+            />
+            <Route
+              exact
+              path="/login"
+              render={(props) => <Login {...props} />}
+            />
           </Switch>
         </Container>
       </BrowserRouter>
@@ -62,12 +86,26 @@ const App = () => {
 
   return (
     <BrowserRouter>
-      <Snackbar anchorOrigin={{ vertical: 'top', horizontal: 'right' }} open={snack} onClose={handleClose} message={snackMSG} />
-      {
-        linear && <div className={classes.linearProgress} style={{ position: 'fixed', top: 0, zIndex: 10000000 }}>
-          <LinearProgress color="secondary" classes={{ colorSecondary: classes.customColor, barColorSecondary: classes.customColor2 }} />
+      <Snackbar
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        open={snack}
+        onClose={handleClose}
+        message={snackMSG}
+      />
+      {linear && (
+        <div
+          className={classes.linearProgress}
+          style={{ position: "fixed", top: 0, zIndex: 10000000 }}
+        >
+          <LinearProgress
+            color="secondary"
+            classes={{
+              colorSecondary: classes.customColor,
+              barColorSecondary: classes.customColor2,
+            }}
+          />
         </div>
-      }
+      )}
       <Container maxWidth="lg">
         <MainMenu />
         <Switch>
@@ -87,11 +125,16 @@ const App = () => {
           <Route path="/requests" render={(props) => <Requests {...props} />} />
           <Route path="/about" render={(props) => <About {...props} />} />
           <Route path="/contact" render={(props) => <Contact {...props} />} />
-          <Route path="/notifications" render={(props) => <Notification {...props} />} />
+          <Route
+            path="/notifications"
+            render={(props) => <Notification {...props} />}
+          />
         </Switch>
       </Container>
     </BrowserRouter>
   );
 };
+
+//hello
 
 export default App;
